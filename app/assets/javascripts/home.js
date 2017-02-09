a8213387ec72cb6529dfa13c0d70010369c81088
justmartin <justin.lamar.martin@gmail.com>
2017-02-08 18:04:10 -0800

    Modified document.querySelector to not use backticks.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
 	window.addEventListener('keypress', addStyle);
 
 	function playSound(e) {
-		var keyCode = (e.keyCode)
-		var audio = document.querySelector(`audio[data-key-code="${keyCode}"]`) 
-
+		var keyCode = e.keyCode
+		var audio = document.querySelector("audio[data-key-code='"+keyCode+"']");
+		console.log(audio);
 		audio.play()
 		audio.currentTime = 0;
 	}
 
 	function addStyle(e) {
 		var keyCode = (e.keyCode)
-		var key = document.querySelector(`.key[data-key-code="${keyCode}"]`) 
+		var key = document.querySelector(".key[data-key-code='"+keyCode+"']"); 
 
 		key.classList.add('playing');
 
